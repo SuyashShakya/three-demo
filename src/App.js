a785fe93f30f085285b3e10382f93fa4ceb5a052
suyash shakya <suyashshakya.h0p3@gmail.com>
2023-07-15 21:17:41 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls} from '@react-three/drei';
 
-function App() {
+const  App = () => {
+  const [size, setSize] = useState([1,1,1])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div style={{display: 'flex', gap: 5, flexDirection: 'column', width: '200px' }}>
+        <input 
+          type='number' 
+          placeholder='height' 
+          onChange={(e) => {
+            let arr = [...size]
+            arr[0] = e.target.value
+            setSize(arr)
+          }}
+        />
+        <input 
+          type='number' 
+          placeholder='width' 
+          onChange={(e) => {
+            let arr = [...size]
+            arr[1] = e.target.value
+            setSize(arr)
+          }}
+        />
+        <input 
+          type='number' 
+          placeholder='depth' 
+          onChange={(e) => {
+            let arr = [...size]
+            arr[2] = e.target.value
+            setSize(arr)
+          }}
+        />
+      </div>
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <OrbitControls />
+        <mesh>
+          <boxGeometry args={size} color='blue'/>
+          <meshStandardMaterial />
+        </mesh>
+        </Canvas>
     </div>
   );
 }
